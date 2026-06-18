d4224a179f103c5935bb6130a37988d697f10d4a
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-18 15:58:58 -0400

    se agrego un mensaje en modal , clase categorias

--- Control/categorias.php
+++ Control/categorias.php
@@ -7,7 +7,7 @@
 	
 	/* Connect To Database*/
 	require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-	require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
+	require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos  "hola"
 	
 	$active_categoria="active";
 	$title="Categorías";
